18b3b5221504a7c8cbea6e7dea07e66e192181e2
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-06 07:34:22 +0530

    Did al the code changes

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-/*  This script demonstrates implementation of the Observer Pattern.
- *  If you're interested in learning about Observer Pattern, 
- *  you can find a dedicated course on Outscal's website.
- *  Link: https://outscal.com/courses
- * */
+/**  This script demonstrates implementation of the Observer Pattern.
+  *  If you're interested in learning about Observer Pattern, 
+  *  you can find a dedicated course on Outscal's website.
+  *  Link: https://outscal.com/courses
+  **/
 
 namespace ServiceLocator.Events
 {
