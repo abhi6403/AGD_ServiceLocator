963efd84bf73d0ad90386d7fe4cf22a7a80570fa
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-02-04 12:35:14 +0530

    Updated Event Service

--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -11,7 +11,6 @@ namespace ServiceLocator.UI
 {
     public class UIService : GenericMonoSingleton<UIService>
     {
-        [SerializeField] private EventService eventService;
 
         [Header("Gameplay Panel")]
         [SerializeField] private GameObject gameplayPanel;
@@ -55,7 +54,7 @@ namespace ServiceLocator.UI
             SubscribeToEvents();
         }
 
-        public void SubscribeToEvents() => eventService.OnMapSelected.AddListener(OnMapSelected);
+        public void SubscribeToEvents() => EventService.Instance.OnMapSelected.AddListener(OnMapSelected);
 
         public void OnMapSelected(int mapID)
         {
